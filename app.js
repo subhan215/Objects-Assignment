6ecd644bc6abaadc895321f04be09ee9b4a696f5
subhan215 <63653805+subhan215@users.noreply.github.com>
2022-01-19 12:45:21 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+/// Answer no 1 ///
+/* var itemsArray = [
+    { name: "juice", price: "50", quantity: "3" },
+    { name: "cookie", price: "30", quantity: "9" },
+    { name: "shirt", price: "80", quantity: "1" },
+    { name: "pe", price: "100", quantity: "2" }];
+var totalPrice1 = itemsArray[0].price * itemsArray[0].quantity;
+var totalPrice2 = itemsArray[1].price * itemsArray[1].quantity;
+var totalPrice3 = itemsArray[2].price * itemsArray[2].quantity;
+var totalPrice4 = itemsArray[3].price * itemsArray[3].quantity;
+var totalPrices = parseInt(totalPrice1 + totalPrice2 + totalPrice3 + totalPrice4);
+    
+
+document.write("Total price of " + itemsArray[0].name  + ":" +  + totalPrice1 + "<br>");
+document.write("Total price of " + itemsArray[1].name  + ":" +  + totalPrice2 + "<br>");
+document.write("Total price of " + itemsArray[2].name  + ":" +  + totalPrice3 + "<br>");
+document.write("Total price of " + itemsArray[3].name  + ":" +  + totalPrice4 + "<br>");
+document.write("Total price of all items: " + totalPrices) */
+/// Answer no 2 ///
+/* var information = {
+    name: "Subhan",
+    email: "subhanmemon324@gmail.com",
+    password: "Subhan12",
+    age: 17,
+    gender: "Male",
+    city: "Karachi",
+    country: "Pakistan"
+}
+console.log("name" in information);
+console.log("country" in information);
+console.log("firstName" in information);
+console.log("lastName" in information);
+ */
